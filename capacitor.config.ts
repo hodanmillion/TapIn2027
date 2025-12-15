56db61d4124f536648139025364d2b035acf2b2d
Hodan Mohamoud <hodanmo15@gmail.com>
2025-12-14 19:00:24 -0500

    Fix TestFlight black screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.TapIn.myapp',
   appName: 'TapIn',
-  webDir: 'public'
+  webDir: 'public',
+  server: {
+    url: 'https://tap-in2026-jj45.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
