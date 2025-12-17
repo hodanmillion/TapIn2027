6180345caeca9d7a6c588dd4ca4ba6665a75dd32
hodanmillion <hodanmo15@gmail.com>
2025-12-17 06:13:33 -0500

    Auto-sync from Orchids Desktop

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.TapIn.myapp',
   appName: 'TapIn',
-  webDir: 'out'
+  webDir: '.next',
+  server: {
+    url: 'https://tap-in2026-jj45-git-main-hodans-projects-65e91166.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
